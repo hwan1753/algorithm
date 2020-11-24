497d758d9e5626385913df9461d82679928f6bd4
hawn1753 <john175258@gmail.com>
2020-11-24 20:44:21 +0900

    Update binary transformation iteration.py

--- binary transformation iteration.py
+++ binary transformation iteration.py
@@ -10,35 +10,33 @@ def solution(s):
         chk_one = sentence.count("1")
         remove_zero += sentence.count("0")
         len_next = chk_one
-        # print("len_next{}".format(chk_one))
-        while len_next > 2:
+
+        while len_next > 1:
 
             new_sentence = str(len_next % 2) + new_sentence
             len_next = len_next // 2
-            # print(len_next)
-        new_sentence = "1" + new_sentence
+
+        if chk_one != 2 and chk_one != 3:
+            new_sentence = "1" + new_sentence
+            change_transform += 1
+        elif chk_one == 3:
+            change_transform += 3
+            remove_zero += 1
+            return [change_transform, remove_zero]
+        else:
+            change_transform += 2
+            remove_zero += 1
+            return [change_transform, remove_zero]
+
         sentence = new_sentence
-        change_transform += 1
-        print(remove_zero)
-        print(change_transform)
-        print(sentence)
-
-
-    if sentence == "11":
-        change_transform += 2
-        remove_zero += 1
-
-    elif sentence == "01":
-        change_transform += 1
-        remove_zero += 1
-
-    elif sentence == "1":
-        change_transform += 1
-
 
     return [change_transform, remove_zero]
 
-s1 = "110010101001"
+
+
+
+
+s1 = "11001010100111"
 print(solution(s1))
 s2 = "01110"
 # print(solution(s2))

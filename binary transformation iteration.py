0c7b59139a405ed4811f23786a10ca568e2d319e
hawn1753 <john175258@gmail.com>
2020-11-24 21:07:34 +0900

    Update binary transformation iteration.py
    Post Blog

--- binary transformation iteration.py
+++ binary transformation iteration.py
@@ -1,35 +1,42 @@
 def solution(s):
 
-    change_transform = 0
-    remove_zero = 0
-    sentence = s
+    change_transform = 0    # 변환 횟수
+    remove_zero = 0         # 0 제거 횟수
+    sentence = s            # input 값. 알고리즘 활용 문자
 
+    # 2진수가 3자리 이상인 경우에 반복문 진행.
     while len(sentence) > 2:
 
-        new_sentence = ""
-        chk_one = sentence.count("1")
-        remove_zero += sentence.count("0")
-        len_next = chk_one
+        new_sentence = ""   # 변환한 문자
+        chk_one = sentence.count("1")   # 1의 갯수, 다음 계산하는 이진수
+        remove_zero += sentence.count("0")  # 제거한 0을 더해줌.
+        len_next = chk_one  # 1의 갯수, 아래 while문에서 활용하는 변수
 
+        # 1의 갯수를 다시 2진수로 변환. 
         while len_next > 1:
 
             new_sentence = str(len_next % 2) + new_sentence
             len_next = len_next // 2
-
+        
+        # 2진수가 3자리 이상인 경우 1을 추가.
         if chk_one != 2 and chk_one != 3:
             new_sentence = "1" + new_sentence
             change_transform += 1
+        
+        # 2진수가 "11" 인 경우 3번 변환과 0을 한번 제거
         elif chk_one == 3:
             change_transform += 3
             remove_zero += 1
             return [change_transform, remove_zero]
+        
+        # 2진수가 "10"인 경우 2번 변환과 0을 한번 제거
         else:
             change_transform += 2
             remove_zero += 1
             return [change_transform, remove_zero]
 
         sentence = new_sentence
-
+    # 2진수가 "1"로 끝난 경우
     return [change_transform, remove_zero]
 
 

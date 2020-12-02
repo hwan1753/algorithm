faa00ba9b3c303ad63c28b5036e6bf30660b9053
hawn1753 <john175258@gmail.com>
2020-12-02 23:55:56 +0900

    Post on Blog

--- jump and  teleport.py
+++ jump and  teleport.py
@@ -1,16 +1,16 @@
 def solution(n):
-    ans = 0
+    ans = 0     # 결과값
 
-    while n > 0:
-        calculation = 1
-        while n >= calculation:
-            tmp = calculation
-            calculation = calculation * 2
+    while n > 0:    # n이 0이 될 때까지
+        calculation = 1     # 2의 제곱으로 계산하는 수
+        while n >= calculation:     # n보다 작은 2의 제곱
+            tmp = calculation       # 가장 큰 2의 제곱 값 저장하기
+            calculation = calculation * 2   # 2의 제곱
 
-        n -= tmp
-        ans += 1
+        n -= tmp    # n보다 작은 가장 큰 2의 제곱 빼주기
+        ans += 1    # 결과값 + 1
 
-    return ans
+    return ans      # 결과값 리턴
 
 a = 5
 b = 6

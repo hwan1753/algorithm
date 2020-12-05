129b575bda063420b44fe62a15f1608b7d50ab91
hawn1753 <john175258@gmail.com>
2020-12-05 17:04:16 +0900

    Create candidate key.py

---
+++ candidate key.py
@@ -0,0 +1,80 @@
+def perm(lst, n):
+    arr = []
+    if n > len(lst):
+        return arr
+
+    if n == 1:
+        for i in lst:
+            arr.append([i])
+    else:
+        for i in range(len(lst)):
+            temp = [val for val in lst]
+            temp.remove(lst[i])
+            for p in perm(temp, n-1):
+                arr.append([lst[i]]+p)
+    return arr
+
+def comb(lst, n):
+    arr = []
+
+    if n > len(lst):
+        return arr
+
+    if n == 1:
+        for i in lst:
+            arr.append([i])
+
+    else:
+        for i in range(len(lst)-n+1):
+            for temp in comb(lst[i+1:], n-1):
+                arr.append([lst[i]] + temp)
+    return arr
+
+
+def solution(relation):
+    row, col = len(relation), len(relation[0])
+    col_arr = [num for num in range(col)]
+
+    candidate_key = []
+    key_list = []
+    result_key = []
+
+    for num in range(1, col+1):
+        key_list.extend(comb(col_arr, num))
+
+
+    for key_col in key_list:
+        check_key = []
+        for rel in range(row):
+            data = ""
+            for num in key_col:
+                data += relation[rel][num]
+            if data not in check_key:
+                check_key.append(data)
+            else:
+                break
+
+        if len(check_key) == row:
+            candidate_key.append(set(key_col))
+
+    for key in candidate_key:
+        tmp = True
+        if not result_key:
+            result_key.append(key)
+        else:
+            for chk in result_key:
+                if chk.issubset(key):
+                    tmp = False
+                    break
+            if tmp:
+                result_key.append(key)
+
+    return len(result_key)
+
+a = [["100", "ryan", "music", "2"], ["200", "apeach", "math", "2"], ["300", "tube", "computer", "3"],
+     ["400", "con", "computer", "4"], ["500", "muzi", "music", "3"], ["300", "apeach", "music", "1"]]
+
+b = [["1","2","3","4"],["1","3","3","4"],["2","2","3","5"],["3","3","4",'4']]
+
+print(solution(a))
+

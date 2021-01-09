512223aa07ba3dbc894bc66166ef42f926982650
hawn1753 <john175258@gmail.com>
2021-01-09 22:22:27 +0900

    Post on Blog

--- BJ9370.py
+++ BJ9370.py
@@ -1,17 +1,25 @@
 from sys import stdin
 import heapq
 
+# 방문여부 확인용 초기값
 INF = 1e9
 
+# 다익스트라 알고리즘
 def dijkstra(start, last):
+    # 방문여부
     dp = [INF] * (n+1)
+    # 시작 값 0부터 시작
     dp[start] = 0
     heap = []
+    # 힙에 (거리값, 정점) push, 우선순위 큐
     heapq.heappush(heap, (dp[start], start))
 
+    # 힙이 있을때까지
     while heap:
+        
         weight, now = heapq.heappop(heap)
         for end, dis in node_dict[now]:
+            # 조건을 만족하는 값 heap정렬
             if dp[end] > weight + dis:
                 dp[end] = weight + dis
                 heapq.heappush(heap, (dp[end], end))
@@ -24,13 +32,16 @@ for _ in range(T):
     s, g, h = map(int, stdin.readline().split())
 
 
+    # 노드 hash화
     node_dict = {}
 
     for _ in range(m):
         a, b, d = map(int, stdin.readline().split())
         node_dict[a] = node_dict.get(a,[]) + [(b,d)]
         node_dict[b] = node_dict.get(b, []) + [(a, d)]
-
+    
+    # 1번 시작 -> g -> h -> 최종
+    # 2번 시작 -> h -> g -> 최종
     answer1, answer2 = 0, 0
 
     answer1 = dijkstra(s, g) + dijkstra(g, h)
@@ -40,13 +51,9 @@ for _ in range(T):
     minimum = INF
     for _ in range(t):
         node = int(stdin.readline())
-        # print("!!")
-        # answer1 += dijkstra(h, node)
-        # answer2 += dijkstra(g, node)
         answer = min(answer1 + dijkstra(h, node), answer2 + dijkstra(g, node))
         chk_answer = dijkstra(s, node)
 
-        # print(answer1 + dijkstra(h, node), answer2 + dijkstra(g, node))
         if chk_answer >= answer:
             heapq.heappush(candidate, node)
 

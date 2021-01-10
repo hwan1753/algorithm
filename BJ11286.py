55633a70f4fe1356b704b9710fba146c5281b485
hawn1753 <john175258@gmail.com>
2021-01-10 22:40:01 +0900

    Post on Blog

--- BJ11286.py
+++ BJ11286.py
@@ -2,14 +2,19 @@ from sys import stdin
 import heapq
 
 N = int(stdin.readline())
+# 우선순위 큐
 heap = []
 
 for n in range(N):
     num = int(stdin.readline())
     if num != 0:
+        # 0이 아닌 경우 우선순위 큐에 (절대값, 숫자) 삽입
         heapq.heappush(heap,(abs(num), num))
+    # 0인경우
     else:
+        # 큐가 비어있으면 0 출력
         if len(heap) == 0:
             print(0)
         else:
+            # 큐에 값이 있으면 pop
             print(heapq.heappop(heap)[1])

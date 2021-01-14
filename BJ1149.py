c7a54c1b9f8761b213b68f7be73b3b8d877830ce
hawn1753 <john175258@gmail.com>
2021-01-14 22:00:30 +0900

    Post on Blog

--- BJ1149.py
+++ BJ1149.py
@@ -1,21 +1,25 @@
 from sys import stdin
-from _collections import deque
 
+# N 개의 줄
 N = int(stdin.readline())
 
+# 배열 RGB값 n개
 arr = [list(map(int, stdin.readline().split())) for _ in range(N)]
 
+# 초기 설정값
 answer = 1e9
-queue = deque([])
 
+# 1번쨰 줄부터 시작
 for n in range(1,N):
     for i in range(3):
+        # 미니멈 초기값
         minimum = 1e9
-
         for j in range(3):
+            # 이전과 같은 색인 경우 제외
+            # 가장 작은 값 찾기
             if i != j and arr[n][i] + arr[n-1][j] < minimum:
                 minimum = arr[n][i] + arr[n-1][j]
-
+        # 가장 작은 값 기록
         arr[n][i] = minimum
 
 print(min(arr[-1]))

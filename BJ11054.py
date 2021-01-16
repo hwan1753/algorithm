1887840c462c3c4b4018c07772722b83fd9c62c3
hawn1753 <john175258@gmail.com>
2021-01-17 03:02:57 +0900

    Update BJ11054.py

--- BJ11054.py
+++ BJ11054.py
@@ -1,39 +1,50 @@
 from sys import stdin
-from bisect import bisect_left, bisect_right
-
+from bisect import bisect_left
 
 N = int(stdin.readline())
 
 arr = list(map(int, stdin.readline().split()))
+
 
 if len(arr) == 1:
     print("1")
 else:
 
     answer = 0
-    chk_idx = []
-    max_val = max(arr)
 
-    for i in range(len(arr)):
-        if arr[i] == max_val:
-            chk_idx.append(i)
+    for idx in range(len(arr)):
 
-
-    while chk_idx:
-        max_idx = chk_idx.pop()
         left_stack, right_stack = [], []
 
-        if max_idx == 0:
-            for i in range(1, len(arr)):
-                if right_stack == []:
+        for i in range(idx):
+            if arr[i] >= arr[idx]:
+                continue
+            elif left_stack == []:
+                left_stack.append(arr[i])
+            else:
+                if left_stack[-1] < arr[i]:
+                    left_stack.append(arr[i])
+                else:
+                    left_stack[bisect_left(left_stack, arr[i])] = arr[i]
+        # print(left_stack)
+
+        for i in range(len(arr) - 1, idx,-1):
+            if arr[i] >= arr[idx]:
+                continue
+            elif right_stack == []:
+                right_stack.append(arr[i])
+            else:
+                if right_stack[-1] < arr[i]:
                     right_stack.append(arr[i])
                 else:
-                    if right_stack[-1] > arr[i]:
-                        right_stack.append(arr[i])
-                    elif right_stack[-1] < arr[i]:
-                        if arr[i] != max_val:
-                            right_stack[bisect_left(right_stack, arr[i]) - 1] = arr[i]
-                            # print(bisect_left(right_stack, arr[i]), arr[i], right_stack)
-        # elif max_idx == N - 1:
-        #     for i in range(n - 2, )
-        print(right_stack)
+                    right_stack[bisect_left(right_stack, arr[i])] = arr[i]
+
+
+        if right_stack:
+            right_stack.append(arr[idx])
+        else:
+            left_stack.append(arr[idx])
+        answer = max(answer, len(left_stack) + len(right_stack))
+        # print(left_stack, right_stack)
+        # print("-" * 10)
+    print(answer)

928cf4fe0b3a579ad7b7fe6996544bf4ed8dbb22
hawn1753 <john175258@gmail.com>
2021-02-02 23:48:21 +0900

    Update 합승 택시 요금.py

--- 합승 택시 요금.py
+++ 합승 택시 요금.py
@@ -1,19 +1,49 @@
 import heapq
 
-def solution(n, s, a, b, fares):
-    answer = 0
-    INF = 1e9
+def dijkstra(node_dict, n, start, end):
 
-    dp = [INF for _ in range(n+1)]
-    node_dict = {i : [] for i in range(1,n+1)}
+    heap = [(0, start)]
+    dp = [1e9 for _ in range(n + 1)]
+    # dp = [1e9] * (n + 1)
+    dp[start] = 0
+
+    while heap:
+        weight, now = heapq.heappop(heap)
+
+        for node, w in node_dict[now]:
+            if dp[node] > weight + w:
+                heapq.heappush(heap, (weight + w, node))
+                dp[node] = weight + w
+    return dp[end]
+
+def solution(n, s, a, b, fares):
+
+    answer = 1e9
+
+    node_dict = [[] for i in range(n+1)]
 
     for start, end, weight in fares:
         node_dict[start].append((end, weight))
         node_dict[end].append((start,weight))
-    print(node_dict)
 
-    heap = [s]
-    dp[s] = 0
+    for i in range(1,n+1):
+
+        if i == s:
+            answer = min(answer, dijkstra(node_dict, n, s, a) + dijkstra(node_dict, n, s, b))
+        elif i == a:
+            dis_a = dijkstra(node_dict, n, s, a)
+            if dis_a < answer:
+                answer = min(answer, dis_a + dijkstra(node_dict, n, a, b))
+
+        elif i == b:
+            dis_b = dijkstra(node_dict, n, s, b)
+            if dis_b < answer:
+                answer = min(answer, dis_b + dijkstra(node_dict, n, b, a))
+        else:
+            dis_i = dijkstra(node_dict, n, s, i)
+            if dis_i < answer:
+                answer = min(answer, dis_i
+                         + dijkstra(node_dict, n, i, a) + dijkstra(node_dict, n, i, b))
 
     return answer
 

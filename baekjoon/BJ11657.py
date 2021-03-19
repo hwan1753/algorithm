35b8a65c03795d8ad2eb4c6a9c24140fbc3d96c5
hawn1753 <john175258@gmail.com>
2021-03-19 23:48:46 +0900

    Create 11657

---
+++ baekjoon/BJ11657.py
@@ -0,0 +1,40 @@
+from sys import stdin
+from _collections import defaultdict
+
+N, M = map(int, stdin.readline().split())
+
+graph = defaultdict(list)
+
+for _ in range(M):
+    start, end, weight = map(int, stdin.readline().split())
+    graph[start] += [[end, weight]]
+
+def chk_inf(distance):
+    for start in graph:
+        for end, weight in graph[start]:
+            if distance[start] != 1e9 and distance[end] > distance[start] + weight:
+                return False
+    return True
+
+
+distance = [1e9] * (N+1)
+distance[1] = 0
+i = 0
+
+for i in range(N-1):
+    for start in graph:
+        for end, weight in graph[start]:
+            
+            if distance[start] != 1e9:
+                distance[end] = min(distance[end], distance[start] + weight)
+                
+
+
+if chk_inf(distance):
+    for i in range(2,len(distance)):
+        if distance[i] == 1e9:
+            print(-1)
+        else:
+            print(distance[i])
+else:
+    print(-1)

fa3b401247cc8735cdca75891a68f28f7a8c4b71
박경환 <gyeonghwanpark@baggyeonghwan-ui-MacBookAir.local>
2021-04-06 00:52:50 +0900

    연구소 3 #골드4 #BFS

--- baekjoon/BJ17142.py
+++ baekjoon/BJ17142.py
@@ -1,5 +1,7 @@
 from sys import stdin
 from _collections import deque
+from itertools import chain
+from pprint import pprint
 
 def comb(lst, num):
 
@@ -7,9 +9,16 @@ def comb(lst, num):
     if num > len(lst):
         return arr
 
-    if len(lst) == 1:
+    if num == 1:
         for i in lst:
             arr.append([i])
+    
+    else:
+        for i in range(len(lst)-num+1):
+            for temp in comb(lst[i+1:],num-1):
+                arr.append([lst[i]]+temp)
+    
+    return arr
 
 
 n, m = map(int, stdin.readline().split())
@@ -19,24 +28,63 @@ board = [list(map(int,stdin.readline().split())) for _ in range(n)]
 dy = (0,1,0,-1)
 dx = (1,0,-1,0)
 
-chk_arr = []
-visit = deque()
-visited = [[1e9]*n for _ in range(n)]
+virus = []
+wall = []
 
 for i in range(n):
     for j in range(n):
         if board[i][j] == 2:
-            visit.append((i,j))
-            visited[i][j] = 0
+            virus.append((i,j))
+        
         if board[i][j] == 1:
-            visited[i][j] = '-'
+            wall.append((i,j))
 
-while visit:
-    y, x = visit.popleft()
-    weight = visited[y][x]
+candidate_arr = comb(virus, m)
+answer = 1e9
 
+if list(chain(*board)).count(0) == 0:
+    answer = 0
+    candidate_arr = []
+
+for candidate in candidate_arr:
+    visit = deque(candidate)
+    visited = [[1e9]*n for _ in range(n)]
+    for i, j in candidate:
+        visited[i][j] = 0
+    for i, j in wall:
+        visited[i][j] = -1
+
+    temp = 0
+
+    while visit:
+        y, x = visit.popleft()
+        weight = visited[y][x]
+
+        
+        for i in range(4):
+            my, mx = y + dy[i], x + dx[i]
+
+            if 0<=my<n and 0<=mx<n:
+
+                if board[my][mx] == 2 and visited[my][mx] == 1e9:
+                    visited[my][mx] = weight + 1
+                    visit.append((my,mx))
+
+                elif board[my][mx] == 1:
+                    visited[my][mx] = -1
+
+                elif visited[my][mx] > weight+1:
+                    visited[my][mx] = weight + 1
+                    visit.append((my,mx))
+                    temp = max(temp, visited[my][mx])
     
-    for i in range(4):
-        my, mx = y + dy[i], x + dx[i]
+    if list(chain(*visited)).count(1e9):
+        pass
+    else:
+        answer = min(answer, temp)
 
-        if 0<=my<n and 0<=mx<n and board[my][mx]
+
+if answer == 1e9:
+    print(-1)
+else:
+    print(answer)
